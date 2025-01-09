a683802a471a839f1bbf744b25dc0008a5ad5f0b
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2025-01-09 21:51:48 +0700

    x695c => radiant
    adapt for orangefox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,34 +1,19 @@
 #!/usr/bin/env bash
+
 #
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+# Copyright (C) 2025 The LineageOS Project
 #
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
+# SPDX-License-Identifier: Apache-2.0
 #
 
-
-FDEVICE="x695c"
-#set -o xtrace
+FDEVICE="radiant"
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then 
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -37,60 +22,86 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
 
-# Dirty Fix: Only declare orangefox vars when needed
-if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]
-then
-	echo -e "\x1b[96mSetting up OrangeFox build vars...\x1b[m"
-	if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   		export TW_DEFAULT_LANGUAGE="en"
-		export LC_ALL="C"
-	 	export OF_FLASHLIGHT_ENABLE=0
-		export ALLOW_MISSING_DEPENDENCIES=true
-		export OF_USE_GREEN_LED=0
-		export OF_HIDE_NOTCH=1
-		export OF_MAINTAINER="Woomymy"
-		export OF_USE_MAGISKBOOT=1
-		export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-		export OF_NO_MIUI_PATCH_WARNING=1
-		export FOX_USE_BASH_SHELL=1
-		export FOX_ASH_IS_BASH=1
-		export FOX_USE_TAR_BINARY=1
-		export FOX_USE_SED_BINARY=1
-		export FOX_USE_XZ_UTILS=1
-		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	    	export OF_USE_TWRP_SAR_DETECT=1
-		export OF_NO_SAMSUNG_SPECIAL=1
-		export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	    export FOX_DELETE_AROMAFM=1
-	    export FOX_ENABLE_APP_MANAGER=1
-		export FOX_USE_NANO_EDITOR=1
-	    # OTA
-	    export OF_KEEP_DM_VERITY=1
-	    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		export OF_USE_NEW_MAGISKBOOT=1
-		export OF_ENABLE_LPTOOLS=1
-		# screen settings
-		export OF_SCREEN_H=2460
-		export OF_STATUS_H=100
-		export OF_ALLOW_DISABLE_NAVBAR=0 # We don't have physical buttons
-		export OF_STATUS_INDENT_LEFT=48
-		export OF_STATUS_INDENT_RIGHT=48
-		export OF_CLOCK_POS=1
-		# maximum permissible splash image size (in kilobytes); do *NOT* increase!
-		export OF_SPLASH_MAX_SIZE=104
-		export OF_PATCH_AVB20=1
-		export FOX_VERSION=R11.1_1
-		# run a process after formatting data to work-around MTP issues
-		export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
-		export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
-		export OF_AB_DEVICE=1
-	export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-		export FOX_USE_SPECIFIC_MAGISK_ZIP="$(gettop)/device/infinix/x695c/Magisk/Magisk.zip"
-	fi
-fi
-#
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    export ALLOW_MISSING_DEPENDENCIES=true
+    export PLATFORM_VERSION_LAST_STABLE="$PLATFORM_VERSION"
+    export PLATFORM_SECURITY_PATCH="2099-12-31"
+    export TW_DEFAULT_LANGUAGE="en"
+    export LC_ALL="C"
 
+    # Partitions Recovery
+    export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+    export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+    export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+
+    # A/B and Dynamic Partitions Support
+    export FOX_AB_SUPPORT=true
+    export OF_AB_DEVICE=1
+    export OF_DYNAMIC_PARTITIONS=true
+    export FOX_VIRTUAL_AB_DEVICE=1
+
+    # OTA and Verity Support
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_FORCED_ENCRYPTION=1
+    export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+ 
+    # AVB Support
+    export OF_PATCH_AVB20=1
+
+    # Magisk Support
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+
+    # Screen Height
+    export OF_SCREEN_H=2400
+    export OF_STATUS_H=80
+    export OF_STATUS_INDENT_LEFT=48
+    export OF_STATUS_INDENT_RIGHT=48
+    export OF_ALLOW_DISABLE_NAVBAR=1
+
+    # Backup and File System Support
+    export OF_QUICK_BACKUP_LIST="/boot;/data;"
+    export OF_USE_TAR_BINARY=1
+    export OF_USE_SED_BINARY=1
+    export OF_USE_XZ_UTILS=1
+
+    # Additional Support 
+    export FOX_DELETE_AROMAFM=1
+    export FOX_ENABLE_APP_MANAGER=1
+    export OF_DEFAULT_KEYMASTER_VERSION="4.0"
+    export FOX_EXTREME_SIZE_REDUCTION=1
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export FOX_DELETE_AROMAFM=1
+    export OF_USE_GREEN_LED=0
+    export FOX_ENABLE_APP_MANAGER=1
+    export OF_ENABLE_LPTOOLS=1
+    export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+    export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
+    export OF_ADVANCED_SECURITY=1
+    export OF_USE_TWRP_SAR_DETECT=1
+   
+    # run a process after formatting data to work-around MTP issues
+    export OF_RUN_POST_FORMAT_PROCESS=1
+    export OF_SKIP_ORANGEFOX_PROCESS=1
+    
+    # R11.1 Settings
+    export FOX_VERSION="R11.0_Q"
+    export FOX_VARIANT="Radiant"
+    export OF_MAINTAINER="Gilanggegea"
+    export FOX_BUILD_DEVICE="Infinix Note 10 Pro"
+    export FOX_BUILD_TYPE="A/B Devices"
+    export TARGET_DEVICE_ALT="X695C"
+
+    # Use LZMA
+    export OF_USE_LZMA_COMPRESSION=1
+
+    # Log Build Variables
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >> "$FOX_BUILD_LOG_FILE"
+        export | grep "OF_" >> "$FOX_BUILD_LOG_FILE"
+        export | grep "TARGET_" >> "$FOX_BUILD_LOG_FILE"
+        export | grep "TW_" >> "$FOX_BUILD_LOG_FILE"
+    fi
+fi
